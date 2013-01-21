eb2c52f2a2cb05c295e6cfa62e05fc022f1931d0
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-21 19:58:00 +0200

    working + more comments

--- puppet/modules/dnsmasq/manifests/init.pp
+++ puppet/modules/dnsmasq/manifests/init.pp
@@ -1,6 +1,9 @@
+# we use dnsmasq to allow wildchar testing for subdomains
+# /etc/hosts is too limited
 class dnsmasq{
   package{"dnsmasq": ensure => installed}
 
+  # all requests to (*.)dawanda.com will go to 127.0.0.1
   file{"/etc/dnsmasq.d/my_local":
     content => "address=/dawanda.com/127.0.0.1\n",
     require => Package["dnsmasq"]
