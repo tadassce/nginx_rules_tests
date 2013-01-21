8b847a97298e48b211241c06316740ba050f9113
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-21 21:24:16 +0200

    dnsmasq as class

--- puppet/modules/dnsmasq/manifests/init.pp
+++ puppet/modules/dnsmasq/manifests/init.pp
@@ -1,11 +1,24 @@
 # we use dnsmasq to allow wildchar testing for subdomains
 # /etc/hosts is too limited
 class dnsmasq{
-  package{"dnsmasq": ensure => installed}
+  class{"dnsmasq::packages":} -> class{"dnsmasq::configs": } -> class{"dnsmasq::service":}
+}
 
+
+class dnsmasq::packages{
+  package{"dnsmasq": ensure => installed}
+}
+
+class dnsmasq::configs{
   # all requests to (*.)dawanda.com will go to 127.0.0.1
   file{"/etc/dnsmasq.d/my_local":
     content => "address=/dawanda.com/127.0.0.1\n",
-    require => Package["dnsmasq"]
+  }
+}
+
+class dnsmasq::service{
+  service{"dnsmasq":
+    ensure    => running,
+    subscribe => Class["dnsmasq::configs"]
   }
 }
