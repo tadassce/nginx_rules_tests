d1c84585649b776335bce0b0fc0a4272040dfaa3
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-21 19:07:33 +0200

    with commment

--- puppet/modules/rewriter_test/templates/app.rb
+++ puppet/modules/rewriter_test/templates/app.rb
@@ -3,10 +3,11 @@ require 'json'
 
 class EnvApp < Sinatra::Base
 
- get '*' do
-   keys =  (request.env.keys.grep(/REQ/) + request.env.keys.grep(/PATH/))
-   r = {}
-   keys.each {|k| r[k] = request.env[k]}
-   JSON.unparse(r)
- end
+  get '*' do
+    #  ["REQUEST_PATH", "REQUEST_URI", "REQUEST_METHOD", "REQUEST_PATH", "PATH_INFO"]
+    keys =  (request.env.keys.grep(/REQ/) + request.env.keys.grep(/PATH/))
+    r = {}
+    keys.each {|k| r[k] = request.env[k]}
+    JSON.unparse(r)
+  end
 end
