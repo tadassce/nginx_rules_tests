89b3e3c6b3b095b04273072ba55907a80a2be86a
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-21 17:21:21 +0200

    stuff

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# run puppet with
+
+sudo puppet apply -vv  --modulepath=/tmp/vagrant-puppet/modules-0/ /tmp/vagrant-puppet/manifests/base.pp
--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -11,6 +11,7 @@ file { '/etc/motd':
 }
 
 
+
 #package{"nginx": ensure => installed}
 package{"dnsmasq": ensure => installed}
 
@@ -18,6 +19,11 @@ package{"dnsmasq": ensure => installed}
 file{"/etc/dnsmasq.d/my_local":
   content => "address=/dawanda.com/127.0.0.1\n",
   require => Package["dnsmasq"]
+}
+
+file{"/usr/local/bin/runpuppet":
+  content => "sudo puppet apply -vv  --modulepath=/tmp/vagrant-puppet/modules-0/ /tmp/vagrant-puppet/manifests/base.pp\n",
+  mode    => 0755
 }
 
 include nginx
